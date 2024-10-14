29c169e4c3060f9d56cbe7f9447bfd4ee0ed8b38
Beto Rocha <roberto.blockchainresources@gmail.com>
2024-10-14 19:32:01 -0300

    Update Lock.sol

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.27;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
@@ -7,6 +7,9 @@ pragma solidity ^0.8.27;
 contract Lock {
     uint public unlockTime;
     address payable public owner;
+
+    error NotInTime();
+    error NotOwner();
 
     event Withdrawal(uint amount, uint when);
 
