6117a3e63f8e9fc8df9c297c7932846a9d77ca79
Beto Rocha <roberto.blockchainresources@gmail.com>
2024-10-15 19:15:17 -0300

    Update Lock.sol

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -27,6 +27,10 @@ contract Lock {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
+        if (block.timestamp < unlockTime) {
+            revert WithdrawNotPossible();
+        }
+
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
         require(msg.sender == owner, "You aren't the owner");
 
