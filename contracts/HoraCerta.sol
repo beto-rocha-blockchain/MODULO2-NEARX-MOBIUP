c1f01623ba22942748278870060a58f0cd46cc31
Beto Rocha <roberto.blockchainresources@gmail.com>
2024-10-15 21:25:49 -0300

    four

--- contracts/HoraCerta.sol
+++ contracts/HoraCerta.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 import "./Professor.sol";
 
@@ -7,25 +7,22 @@ contract HoraCerta {
 
     mapping(address => Professor) public professores;
 
-    event ProfessorRegistrado(address, Professor);
+    event ProfessorRegistrado(address profAddress, Professor professor);
 
     error HoraNaoValida();
 
-    function registrarProfessor(address _profAddress, Professor memory _profesor) public {
+    function registrarProfessor(address _profAddress, Professor memory _professor) public {
         professores[_profAddress] = _professor;
         emit ProfessorRegistrado(_profAddress, _professor);
     }
 
-    function mudarNomeProfessor(address _profAddress, int _horas) public {
+    function mudarNomeProfessor(address _profAddress, string memory _nome) public {
         professores[_profAddress].nome = _nome;
     }
 
-    function registrarHora(address _profAddress, int _horas) public {
-        if (_horas < 0) {
-            revert HoraNaoValida();
-        }
-
-        professores[_profAddress].horasTrabalhadas += uint(_horas);
+    function registrarHora(address _profAddress, uint _horas) public {
+        // Não precisa verificar se _horas é negativo, pois é um uint
+        professores[_profAddress].horasTrabalhadas += _horas;
     }
 
 }
